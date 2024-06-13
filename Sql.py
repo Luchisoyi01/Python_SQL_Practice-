5f1fe76d746b77b687ef5db4d80cbd0a9a14c1d4
Luchisoyi01 <117387787+Luchisoyi01@users.noreply.github.com>
2024-06-13 22:02:31 +0300

    Added some data in the database

--- Sql.py
+++ Sql.py
@@ -9,7 +9,20 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+sql= "INSERT INTO students (name, age) VALUES (%s , %s)"
+students=[("John", 22),
+          ("Wendy", 20),
+          ("Moraa", 17),
+          ("Joy", 21),
+          ("Ann", 22)]
+
+
+mycursor.executemany(sql, students)
+
+mydb.commit()
+
+#mycursor.execute("ALTER TABLE students ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
 #mycursor.execute("CREATE TABLE Students (name VARCHAR(255), age INTEGER(10))")
-mycursor.execute("SHOW TABLES")
-for TB in mycursor:
-  print(TB)
+#mycursor.execute("SHOW TABLES")
+#for TB in mycursor:
+ # print(TB)
